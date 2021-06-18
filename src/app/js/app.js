465749f3ebf760536d86c6238565a2d830f2dfbe
delvin02 <64721581+delvin02@users.noreply.github.com>
2021-06-19 03:11:30 +0800

    edit

--- src/app/js/app.js
+++ src/app/js/app.js
@@ -146,7 +146,7 @@
     var $typed = $("#typed");
     if ($typed.length) {
       var typed = new Typed("#typed", {
-        strings: ["Web Developer", "Freelancer", "Photographer", "Student", "Tech-savy"],
+        strings: ["Web Developer", "Freelancer", "Photographer", "Student", "Tech-savvy"],
         loop: true,
         typeSpeed: 80
       });
